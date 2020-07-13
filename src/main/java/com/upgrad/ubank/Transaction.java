7c0831d4969775b56fbb0ecc61583a49d6fc2e62
Ishwar Soni <ishwar.soni@upgrad.com>
2020-07-13 20:58:54 +0530

    demo for comment why, not what

--- src/main/java/com/upgrad/ubank/Transaction.java
+++ src/main/java/com/upgrad/ubank/Transaction.java
@@ -1,4 +1,45 @@
 package com.upgrad.ubank;
 
 public class Transaction {
+    private int accountNo;
+    private String date;
+    private String action;
+    private int amount;
+
+    public int getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return date + '\t' + action + '\t' + amount;
+    }
 }
