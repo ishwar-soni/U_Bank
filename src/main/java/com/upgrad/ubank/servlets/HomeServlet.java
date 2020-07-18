6db9c5b303e033c558a74df33359652c5c2e1416
Ishwar Soni <ishwar.soni@upgrad.com>
2020-07-18 22:23:14 +0530

    accessing all the parameters

--- src/main/java/com/upgrad/ubank/servlets/HomeServlet.java
+++ src/main/java/com/upgrad/ubank/servlets/HomeServlet.java
@@ -5,10 +5,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("Learn @ Upgrad");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Enumeration<String> params = req.getParameterNames();
+        while (params.hasMoreElements()) {
+            String param = params.nextElement();
+            String value = req.getParameter(param);
+            resp.getWriter().println(param + ": " + value);
+        }
     }
 }
