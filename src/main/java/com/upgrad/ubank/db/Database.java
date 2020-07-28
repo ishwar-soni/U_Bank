9f1a23dda9a5ff078fe8b4e63fe28bb1e0cc8954
Ishwar Soni <ishwar.soni@upgrad.com>
2020-07-28 18:56:05 +0530

    added database.config file

--- src/main/java/com/upgrad/ubank/db/Database.java
+++ src/main/java/com/upgrad/ubank/db/Database.java
@@ -1,23 +1,51 @@
 package com.upgrad.ubank.db;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Database {
     private static Connection connection = null;
 
     public static Connection getConnection () {
         if (connection == null) {
-            String url = "jdbc:oracle:thin:@localhost:1521:xe";
-            String username = "ishwar";
-            String password = "oracle";
+            BufferedReader br = null;
+            Map<String, String> credentials = null;
+            try {
+                String path = System.getProperty("catalina.base")+"\\..\\..\\src\\main\\java\\com\\upgrad\\ubank\\db\\database.config";;
+                br = new BufferedReader(new FileReader(path));
+                credentials = new HashMap<>();
+                String line;
+                while ((line=br.readLine()) != null) {
+                    String[] tokens = line.split("=");
+                    credentials.put(tokens[0], tokens[1]);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Config file not found.");
+            } catch (IOException e) {
+                System.out.println("Error while reading config file.");
+            } finally {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    System.out.println("Error while closing stream.");
+                }
+            }
+            String url = credentials.get("url");
+            String username = credentials.get("username");
+            String password = credentials.get("password");
 
             try {
-                Class.forName("oracle.jdbc.driver.OracleDriver");
+                Class.forName(credentials.get("driverName"));
                 connection = DriverManager.getConnection(url, username, password);
             } catch (ClassNotFoundException e) {
-                System.out.println("Oracle Driver not found. Please download and add the driver.");
+                System.out.println(credentials.get("errorMessage"));
             } catch (SQLException e) {
                 e.printStackTrace();
             }
