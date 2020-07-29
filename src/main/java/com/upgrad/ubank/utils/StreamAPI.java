e9248b76bf5ded44f7c1d2873f27388259151c41
Ishwar Soni <ishwar.soni@upgrad.com>
2020-07-29 22:19:19 +0530

    map() method

--- src/main/java/com/upgrad/ubank/utils/StreamAPI.java
+++ src/main/java/com/upgrad/ubank/utils/StreamAPI.java
@@ -18,6 +18,12 @@ public class StreamAPI {
                 .forEach(System.out::println);
     }
 
+    public static void printAmounts () {
+        transactions.stream()
+                .map(Transaction::getAmount)
+                .forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
         transactions.add(new Transaction(1234, "29/07/2020", "DEPOSIT", 10000));
         transactions.add(new Transaction(1234, "29/07/2020", "WITHDRAW", 5000));
@@ -28,6 +34,6 @@ public class StreamAPI {
         transactions.add(new Transaction(5678, "31/07/2020", "DEPOSIT", 12000));
         transactions.add(new Transaction(5678, "01/08/2020", "DEPOSIT", 1000));
 
-        filterByAction();
+        printAmounts();
     }
 }
