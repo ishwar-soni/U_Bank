d22d6f8d42aad0c7c2eb38a4d962b46be0d6adad
Ishwar Soni <ishwar.soni@upgrad.com>
2020-07-30 08:14:03 +0530

    collect () method

--- src/main/java/com/upgrad/ubank/utils/StreamAPI.java
+++ src/main/java/com/upgrad/ubank/utils/StreamAPI.java
@@ -5,6 +5,7 @@ import com.upgrad.ubank.dtos.Transaction;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamAPI {
     public static List<Transaction> transactions = new ArrayList<>();
@@ -37,6 +38,15 @@ public class StreamAPI {
         System.out.println(sum);
     }
 
+    public static List<Integer> getSortedDepositAmountByAccountNo (int accountNo) {
+        return transactions.stream()
+                .filter(t -> t.getAccountNo() == accountNo)
+                .filter(t -> t.getAction().equals("DEPOSIT"))
+                .map(Transaction::getAmount)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         transactions.add(new Transaction(1234, "29/07/2020", "DEPOSIT", 10000));
         transactions.add(new Transaction(1234, "29/07/2020", "WITHDRAW", 5000));
@@ -47,6 +57,6 @@ public class StreamAPI {
         transactions.add(new Transaction(5678, "31/07/2020", "DEPOSIT", 12000));
         transactions.add(new Transaction(5678, "01/08/2020", "DEPOSIT", 1000));
 
-        printAmountSum();
+        System.out.println(getSortedDepositAmountByAccountNo(1234));
     }
 }
