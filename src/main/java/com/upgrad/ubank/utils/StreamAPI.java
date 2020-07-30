73a92e33fb83ffbc7a09266ada74017ace192cfc
Ishwar Soni <ishwar.soni@upgrad.com>
2020-07-30 07:22:50 +0530

    sorter() method

--- src/main/java/com/upgrad/ubank/utils/StreamAPI.java
+++ src/main/java/com/upgrad/ubank/utils/StreamAPI.java
@@ -3,6 +3,7 @@ package com.upgrad.ubank.utils;
 import com.upgrad.ubank.dtos.Transaction;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamAPI {
@@ -24,6 +25,12 @@ public class StreamAPI {
                 .forEach(System.out::println);
     }
 
+    public static void printTransactionByIncreasingAmount () {
+        transactions.stream()
+                .sorted(Comparator.comparing(Transaction::getAmount))
+                .forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
         transactions.add(new Transaction(1234, "29/07/2020", "DEPOSIT", 10000));
         transactions.add(new Transaction(1234, "29/07/2020", "WITHDRAW", 5000));
@@ -34,6 +41,6 @@ public class StreamAPI {
         transactions.add(new Transaction(5678, "31/07/2020", "DEPOSIT", 12000));
         transactions.add(new Transaction(5678, "01/08/2020", "DEPOSIT", 1000));
 
-        printAmounts();
+        printTransactionByIncreasingAmount();
     }
 }
